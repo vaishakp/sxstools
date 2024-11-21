ca9f08388f7fe8bc0b478965adfffcfaacb4e3f2
Vaishak Prasad <vaishakprasad@gmail.com>
2024-11-21 12:37:52 +0530

    add sxs data loader

--- sxstools/data_loader.py
+++ sxstools/data_loader.py
@@ -5,26 +5,36 @@ from pathlib import Path
 from sxstools.transforms import GetSphereRadialExtents, GetDomainRadii
 from spectral.spherical.grids import GLGrid
 from spectral.chebyshev.chebyshev import ChebyshevSpectral
+import os
+from collections import namedtuple
+
 
 class SXSDataLoader:
 
-
     def __init__(self,
                  run_dir,
-                 name='SphereC0',
+                 subdomain="SphereC0",
                  metric_dir='PsiKappa'):
 
         self.run_dir = Path(run_dir)
         self._data = {}
-        self.name = name
+        self.subdomain = subdomain
         self.metric_dir = metric_dir
+
+
 
     @property
     def data(self):
         return self._data
 
     def load_grid_structure(self):
-        filev = self.run_dir/f"{self.metric_dir}/Vars_{self.name}.h5"
+
+        #files = os.listdir(self.run_dir/f"{self.metric_dir}")
+        #filev_suffix = [item for item in files if self.subdomain in item]
+
+        #assert len(filev_suffix)==1, f"Multiple {self.subdomain}.h5 files found! {filev_suffix}"
+        
+        filev = self.run_dir/f"{self.metric_dir}/DumpedMetricData_{self.subdomain}.h5"
         vars_dat = h5py.File(filev)
         self.n_radii, self.n_theta, self.n_phi = vars_dat['psi']['Step000000'].attrs['Extents']
         self.n_time = len(list(vars_dat['psi'].keys()))
@@ -37,30 +47,46 @@ class SXSDataLoader:
 
         message(f"Available radii data {radii_dict.keys()}")
         
-        try:
-            self.radial_collocation_points = radii[self.name]
-            self.r_min, self.r_max = GetSphereRadialExtents(radii_dict, sub_domain=self.name)
-        except Exception as excep:
-            message(f" {excep}: \t Radial collocation points data not found for SubDomain {self.name}. Retreiving from SphereC0")
+        #try:
+        self.radial_collocation_points = radii_dict[self.subdomain[:-1]]
+        self.r_min, self.r_max = GetSphereRadialExtents(radii_dict, sub_domain=self.subdomain)
+
+
+        #except Exception as excep:
+        #    message(excep, f"\t Radial collocation points data not found for SubDomain {self.subdomain[:-1]}. Retreiving from SphereC0")
             
-            self.radial_collocation_points = radii["SphereC"]
-            self.r_min, self.r_max = GetSphereRadialExtents(radii_dict, sub_domain="SphereC0")
+        #    self.radial_collocation_points = radii_dict["SphereC"]
+        #    self.r_min, self.r_max = GetSphereRadialExtents(radii_dict, sub_domain="SphereC0")
 
-        assert len(self.radial_collocation_points) == self.n_radii, "Number of radial collocation points obtained from GrDomain.input does not match that from the data!"
+        self.construct_angular_grid()
+        self.construct_radial_grid()
 
-    def get_angular_grid(self):
+    def construct_angular_grid(self):
 
-        self.Grid = GLGrid(L=self.n_theta-1)
+        self.AngularGrid = GLGrid(L=self.n_theta-1)
+        self.theta_grid, self.phi_grid = self.AngularGrid.meshgrid
 
-    def get_radial_grid(self):
+    def construct_radial_grid(self):
         
-        self.radial_grid = ChebyshevSpectral(
+        self.RadialGrid = ChebyshevSpectral(
             a=self.r_min, b=self.r_max, Nfuncs=self.n_radii
         )
-
-    def get_four_metric_data(self, t_step):
-        pass
 
     def load_Psi4_data(self):
         pass
 
+
+    def get_four_metric(self, t_step=0, component='tt'):
+        
+        fpath = self.run_dir/f"{self.metric_dir}/DumpedMetricData_{self.subdomain}.h5"
+        dat_file = h5py.File(fpath)
+        t_key = f"Step{str(t_step).zfill(6)}"
+
+        reqd_data = dat_file['psi'][t_key][component][...].reshape(self.n_phi, self.n_theta, self.n_radii).T
+        dat_file.close()
+
+        return reqd_data 
+
+    def get_derivative_four_metric(self, t_step=0, component='ttt'):
+        
+        pass
