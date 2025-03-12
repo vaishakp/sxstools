791fd229a5d7cc125a466a8e843901dc2aa73810
Vaishak Prasad <vaishakprasad@gmail.com>
2025-03-13 01:41:36 +0530

    implement frame transforms to t_ref

--- sxstools/coordinate_transform_interp.py
+++ sxstools/coordinate_transform_interp.py
@@ -14,10 +14,10 @@ class CoordinateTransform:
     1. Specify an SXS WaveformModes obj as waveform_modes or just the modes and its time axis
     2. The choice of z axis to compute and use: Orbital angular momentum or normal to rotation plane
     3. If Omega_hat is chosen, one can spcify which object's angular momentum to use.
-    4. Choose fine alignment (by interpolation) or a rough one (by the closest time stamp)
 
-    The last three have been retained for legacy comparisons with surragate_modelling implementation.
+    The last two have been retained for legacy comparisons with surragate_modelling choices.
 
+    Although can be easily added, these transformations are carr
     '''
 
     def __init__(self,
@@ -33,8 +33,7 @@ class CoordinateTransform:
                  v_kick,
                  normal_direction='Lhat',
                  Omegahat_choice='AhA',
-                 waveform_times=None,
-                 method='fine'
+                 waveform_times=None
                  ):
         
         self.t_ref = t_ref
@@ -70,28 +69,6 @@ class CoordinateTransform:
         self.n_hor_times = len(self.horizon_times)
         self.normal_direction=normal_direction
         self.Omegahat_choice = Omegahat_choice
-        self.method = method
-
-        if self.method=='fine':
-            self.xA_t = self.xA_t_fine
-            self.xB_t = self.xB_t_fine
-            self.dxA_t = self.dxA_t_fine
-            self.dxB_t = self.dxB_t_fine
-            self.xA_rot_z_t = self.xA_rot_z_t_fine
-            self.xB_rot_z_t = self.xB_rot_z_t_fine
-            self.dxA_rot_z_t = self.dxA_rot_z_t_fine
-            self.dxB_rot_z_t = self.dxB_rot_z_t_fine
-        else:
-            self.xA_t = self.xA_t_rough
-            self.xB_t = self.xB_t_rough
-            self.dxA_t = self.dxA_t_rough
-            self.dxB_t = self.dxB_t_rough
-            self.xA_rot_z_t = self.xA_rot_z_t_rough
-            self.xB_rot_z_t = self.xB_rot_z_t_rough
-            self.dxA_rot_z_t = self.dxA_rot_z_t_rough
-            self.dxB_rot_z_t = self.dxB_rot_z_t_rough
-        
-        self.construct_interpolants()
 
         # To hold the result
         self.transformed_quantities = {}
@@ -99,78 +76,55 @@ class CoordinateTransform:
     
     def construct_interpolants(self):
         ''' Construct interpolants for variables '''
-
-        if self.method=='fine':
-            self._xA_interpolant = self.interpolate(self.horizon_times, self.xA)
-            self._xB_interpolant = self.interpolate(self.horizon_times, self.xB)
-            self._chiA_interpolant = self.interpolate(self.horizon_times, self.chiA)
-            self._chiB_interpolant = self.interpolate(self.horizon_times, self.chiB)
+        self._xA_interpolant = self.interpolate(self.horizon_times, self.xA)
+        self._xB_interpolant = self.interpolate(self.horizon_times, self.xB)
+        self._chiA_interpolant = self.interpolate(self.horizon_times, self.chiA)
+        self._chiB_interpolant = self.interpolate(self.horizon_times, self.chiB)
+        #self._massA_interpolant = self.interpolate(self.horizon_times, self.massA)
+        #self._massB_interpolant = self.interpolate(self.horizon_times, self.massB)
 
     def construct_interpolants_rot_z(self):
         ''' Construct interpolants for variables '''
-        if self.method=='fine':
-            self._xA_rot_z_interpolant = self.interpolate(self.horizon_times, self.xA_rot_z)
-            self._xB_rot_z_interpolant = self.interpolate(self.horizon_times, self.xB_rot_z)
+        self._xA_rot_z_interpolant = self.interpolate(self.horizon_times, self.xA_rot_z)
+        self._xB_rot_z_interpolant = self.interpolate(self.horizon_times, self.xB_rot_z)
+
+    def massAt(self, t_ref):
+        return np.array(self._massA_interpolant(t_ref))
     
-    def xA_t_fine(self, t_ref):
+    def massBt(self, t_ref):
+        return np.array(self._massB_interpolant(t_ref))
+
+    def xAt(self, t_ref):
         return np.array([self._xA_interpolant[idx](t_ref) for idx in range(3)])
 
-    def xB_t_fine(self, t_ref):
+    def xBt(self, t_ref):
         return np.array([self._xB_interpolant[idx](t_ref) for idx in range(3)])
 
-    def xA_t_rough(self, t_ref=None):
-        return self.xA[self.t_ref_idx_horizon]
-    
-    def xB_t_rough(self, t_ref=None):
-        return self.xB[self.t_ref_idx_horizon]
-    
-    def dxA_t_fine(self, t_ref):
+    def dxAt(self, t_ref):
         return np.array([self._xA_interpolant[idx].derivative()(t_ref) for idx in range(3)])
 
-    def dxB_t_fine(self, t_ref):
+    def dxBt(self, t_ref):
         return np.array([self._xB_interpolant[idx].derivative()(t_ref) for idx in range(3)])
-    
-    def dxA_t_rough(self, t_ref=None):
-        return np.diff(self.xA, axis=0)[self.t_ref_idx_horizon]
-    
-    def dxB_t_rough(self, t_ref=None):
-        return np.diff(self.xB, axis=0)[self.t_ref_idx_horizon]
-    
-    def xA_rot_z_t_fine(self, t_ref):
+
+    def xA_rot_z_t(self, t_ref):
         return np.array([self._xA_rot_z_interpolant[idx](t_ref) for idx in range(3)])
 
-    def xB_rot_z_t_fine(self, t_ref):
+    def xB_rot_z_t(self, t_ref):
         return np.array([self._xB_rot_z_interpolant[idx](t_ref) for idx in range(3)])
-    
-    def xA_rot_z_t_rough(self, t_ref=None):
-        return self.xA_rot_z[self.t_ref_idx_horizon]
-    
-    def xB_rot_z_t_rough(self, t_ref=None):
-        return self.xB_rot_z[self.t_ref_idx_horizon]
-    
-    def dxA_rot_z_t_fine(self, t_ref):
+
+    def dxA_rot_z_t(self, t_ref):
         return np.array([self._xA_rot_z_interpolant[idx].derivative()(t_ref) for idx in range(3)])
 
-    def dxB_rot_z_t_fine(self, t_ref):
+    def dxB_rot_z_t(self, t_ref):
         return np.array([self._xB_rot_z_interpolant[idx].derivative()(t_ref) for idx in range(3)])
 
-    def dxA_rot_z_t_rough(self, t_ref=None):
-        return np.diff(self.xA_rot_z, axis=0)[self.t_ref_idx_horizon]
-
-    def dxB_rot_z_t_rough(self, t_ref=None):
-        return np.diff(self.xB_rot_z, axis=0)[self.t_ref_idx_horizon]
-
-    def compute_reference_values(self):
-
-        self.chiA_ref = np.array([self.interpolate(self.horizon_times, self.chiA_rot_xyz)[idx](self.t_ref) for idx in range(3)])
-        self.chiB_ref = np.array([self.interpolate(self.horizon_times, self.chiB_rot_xyz)[idx](self.t_ref) for idx in range(3)])
 
     def interpolate(self, time, data):
         _, n_cols = data.shape
 
         interpolants = []
         for idx in range(n_cols):
-            interpolants.append(InterpolatedUnivariateSpline(time, data[:, idx], k=5))
+            interpolants.append(InterpolatedUnivariateSpline(time, data[:, idx]))
         
         return interpolants
 
@@ -181,14 +135,14 @@ class CoordinateTransform:
         # Compute angular momentum (ignore dt for direction)
         #dxA = np.diff(self.xA, axis=0)[self.t_ref_idx_horizon]
         #dxB = np.diff(self.xB, axis=0)[self.t_ref_idx_horizon]
-        dxA = self.dxA_t(self.t_ref)
-        dxB = self.dxB_t(self.t_ref)
-        pAdt = self.massA*dxA
-        pBdt = self.massB*dxB
+        dxA = self.dxAt(self.t_ref)
+        dxB = self.dxBt(self.t_ref)
+        pAdt = self.massAt(self.t_ref)*dxA
+        pBdt = self.massBt(self.t_ref)*dxB
         #lAdt = np.cross(self.xA[self.t_ref_idx_horizon], pAdt)
         #lBdt = np.cross(self.xB[self.t_ref_idx_horizon], pBdt)
-        lAdt = np.cross(self.xA_t(self.t_ref), pAdt)
-        lBdt = np.cross(self.xB_t(self.t_ref), pBdt)
+        lAdt = np.cross(self.xAt(self.t_ref), pAdt)
+        lBdt = np.cross(self.xBt(self.t_ref), pBdt)
         Ldt = lAdt + lBdt
         self.Lhat = Ldt/np.sqrt((np.dot(Ldt, Ldt)))
 
@@ -199,12 +153,12 @@ class CoordinateTransform:
         # Compute rotation plane individually
         dxA = np.diff(self.xA, axis=0)[self.t_ref_idx_horizon]
         dxB = np.diff(self.xB, axis=0)[self.t_ref_idx_horizon]
-        #dxA = self.dxA_t(self.t_ref)
-        #dxB = self.dxB_t(self.t_ref)
+        #dxA = self.dxAt(self.t_ref)
+        #dxB = self.dxBt(self.t_ref)
         omegaAdt = np.cross(self.xA[self.t_ref_idx_horizon], dxA)
         omegaBdt = np.cross(self.xB[self.t_ref_idx_horizon], dxB)
-        #omegaAdt = np.cross(self.xA_t(self.t_ref), dxA)
-        #omegaBdt = np.cross(self.xB_t(self.t_ref), dxB)
+        #omegaAdt = np.cross(self.xAt(self.t_ref), dxA)
+        #omegaBdt = np.cross(self.xBt(self.t_ref), dxB)
         omegaAhat = omegaAdt/np.sqrt((np.dot(omegaAdt, omegaAdt)))
         omegaBhat = omegaBdt/np.sqrt((np.dot(omegaBdt, omegaBdt)))
 
@@ -254,7 +208,7 @@ class CoordinateTransform:
                                                         self.waveform_modes_data, 
                                                         inverse=1)
 
-        self.construct_interpolants_rot_z()
+        self.construct_interpolants()
 
 
     def align_along_xy(self):
@@ -292,11 +246,8 @@ class CoordinateTransform:
         ''' Compute the orbital phasing between the compact objects
         in the coordinate system aligned with Lhat at t_ref '''
 
-        #dX =  self.xA_rot_z[self.t_ref_idx_horizon,0] -self.xB_rot_z[self.t_ref_idx_horizon,0] 
-        #dY =  self.xA_rot_z[self.t_ref_idx_horizon,1] -self.xB_rot_z[self.t_ref_idx_horizon,1]
-        dX =  self.xA_rot_z_t(self.t_ref)[0] -self.xB_rot_z_t(self.t_ref)[0] 
-        dY =  self.xA_rot_z_t(self.t_ref)[1] -self.xB_rot_z_t(self.t_ref)[1]
-
+        dX =  self.xA_rot_z[self.t_ref_idx_horizon,0] -self.xB_rot_z[self.t_ref_idx_horizon,0] 
+        dY =  self.xA_rot_z[self.t_ref_idx_horizon,1] -self.xB_rot_z[self.t_ref_idx_horizon,1]
         self.phi_ref = np.angle(dX + 1j*dY)
         
     def compute_orbital_phase_legacy(self):
@@ -333,11 +284,11 @@ class CoordinateTransform:
         
         self.align_along_z()
         self.align_along_xy()
-        self.compute_reference_values()
-
         keys = [ key for key in self.__dict__.keys() if 'xyz' in key]
 
         for key, val in self.__dict__.items():
-            if ('xyz' in key) or ('ref' in key):
+            if 'xyz' in key:
                 self.transformed_quantities.update({key : val})
 
+    def get_value_at_t_ref(self):
+        ''' Interpolate the given data onto the given time step '''
